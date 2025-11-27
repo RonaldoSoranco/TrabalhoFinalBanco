6ddea80b0a5e5264b3fc9fe594975d4cc90869cc
Samuel <s4mualo@gmail.com>
2025-11-27 11:41:40 -0300

    Criacao do arquivo transacao response onde vai ser a saida da API de transacao e implementacao dos dtos (entradas e saidas da api de conta,transacao e usuario

--- bancos/src/main/java/br/com/cesurgmarau/bancos/dto/ContaResponse.java
+++ bancos/src/main/java/br/com/cesurgmarau/bancos/dto/ContaResponse.java
@@ -1,4 +1,45 @@
 package br.com.cesurgmarau.bancos.dto;
 
+import br.com.cesurgmarau.bancos.model.Usuario;
+
+import java.math.BigDecimal;
+
 public class ContaResponse {
+
+    private Long id;
+    private String numero;
+    private BigDecimal saldo;
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- bancos/src/main/java/br/com/cesurgmarau/bancos/dto/TransacaoRequest.java
+++ bancos/src/main/java/br/com/cesurgmarau/bancos/dto/TransacaoRequest.java
@@ -1,4 +1,36 @@
 package br.com.cesurgmarau.bancos.dto;
 
+import java.math.BigDecimal;
+
 public class TransacaoRequest {
+
+private BigDecimal valor;
+
+private Long contaOrigemId;
+
+private Long contaDestinoId;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Long getContaOrigemId() {
+        return contaOrigemId;
+    }
+
+    public void setContaOrigemId(Long contaOrigemId) {
+        this.contaOrigemId = contaOrigemId;
+    }
+
+    public Long getContaDestinoId() {
+        return contaDestinoId;
+    }
+
+    public void setContaDestinoId(Long contaDestinoId) {
+        this.contaDestinoId = contaDestinoId;
+    }
 }
